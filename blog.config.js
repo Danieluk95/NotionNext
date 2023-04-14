6f130b0fde609e0cdf50861f93cd914d0dd17cba
Danieluk95 <112538517+Danieluk95@users.noreply.github.com>
2023-04-14 14:14:54 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Soko', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '花束を花の帰るべき場所に返してあげよう', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion.danieluk.cn/', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion,博客,blog', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'Lukyebun@outlook.com', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
